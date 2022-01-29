768f039cc45b08da036ffa3f6f3b2e6deec904c6
Михаил Сюрсин <mixail.syursin@gmail.com>
2022-01-29 11:20:51 +0300

    init

---
+++ php/app/src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function main(): \Symfony\Component\HttpFoundation\JsonResponse
+    {
+        return $this->json([]);
+    }
+}
