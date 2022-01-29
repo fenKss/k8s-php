cbbca60ded274a06de82020ce666b380b7c3d5bb
Михаил Сюрсин <mixail.syursin@gmail.com>
2022-01-29 11:44:40 +0300

    probes

--- php/app/src/Controller/MainController.php
+++ php/app/src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,5 +14,13 @@ class MainController extends AbstractController
     public function main(): \Symfony\Component\HttpFoundation\JsonResponse
     {
         return $this->json(['123']);
+    }
+
+    /**
+     * @Route("/health", name="health")
+     */
+    public function health(): Response
+    {
+        return new Response('ok');
     }
 }
